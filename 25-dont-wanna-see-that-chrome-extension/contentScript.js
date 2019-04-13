4920ec8fb57fd9022006be377caadbf0b3a6240f
AhmedAGadir <ahmed.gadir@gmail.com>
2019-04-13 12:56:00 +0200

    update

---
+++ 25-dont-wanna-see-that-chrome-extension/contentScript.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Dont Wanna See That!');
+    // console.log(document.querySelectorAll('.row')[1]);
+    // document.querySelectorAll('.row')[1].style.display = 'none';
+    // document.querySelector('.ui.inverted.fixed.top.borderless.menu').style.backgroundColor = 'blue';
+})
